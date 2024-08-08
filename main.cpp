11dc16613a89088427ee8694740e8c3d2183c648
Marcus Larsson <marcuslarsson_92@hotmail.com>
2024-08-08 11:09:35 +0900

    ..

--- main.cpp
+++ main.cpp
@@ -4,34 +4,36 @@
 
 #include "Windows.h"
 
-int exec_command_line_silent(const char *fmt, ...) {
-    int cmd_cap = 64 << 20;
-    char *cmd_line = (char *)malloc(cmd_cap);
+int exec_command_line(const char *format, ...) {
+    int command_cap = 64 << 20;
+    char *command_line = (char *)malloc(command_cap);
 
     va_list va;
-    va_start(va, fmt);
-    vsprintf(cmd_line, fmt, va);
+    va_start(va, format);
+    vsprintf(command_line, format, va);
     va_end(va);
 
     STARTUPINFO start_info = {};
-    PROCESS_INFORMATION process_info = {};
-
+    start_info.cb = sizeof(STARTUPINFO);
     start_info.dwFlags = STARTF_USESTDHANDLES;
+    start_info.hStdInput = INVALID_HANDLE_VALUE;
+    start_info.hStdOutput = INVALID_HANDLE_VALUE;
+    start_info.hStdError = INVALID_HANDLE_VALUE;
 
     int exit_code;
+    PROCESS_INFORMATION process_info = {};
 
-    if (CreateProcess(NULL, cmd_line, NULL, NULL, true, 0, NULL, NULL, &start_info, &process_info)) {
+    if (CreateProcess(NULL, command_line, NULL, NULL, true, 0, NULL, NULL, &start_info, &process_info)) {
         WaitForSingleObject(process_info.hProcess, INFINITE);
         GetExitCodeProcess(process_info.hProcess, (DWORD *)&exit_code);
-
         CloseHandle(process_info.hProcess);
         CloseHandle(process_info.hThread);
     } else {
-        printf("Failed to execute command:\n\t%s\n", cmd_line);
+        printf("Failed to execute command:\n\t%s\n", command_line);
         exit_code = -1;
     }
 
-    free(cmd_line);
+    free(command_line);
 
     return exit_code;
 }
@@ -57,7 +59,9 @@ int main() {
     lib_free(&lib);
 
     {
-        exec_command_line_silent("link.exe tests/test.lib /MACHINE:X64 /DLL /EXPORT:greeting /NOIMPLIB /NOEXP /NOENTRY /OUT:temp.dll");
+        exec_command_line("link.exe %s /MACHINE:X64 /DLL %s /NOIMPLIB /NOEXP /NOENTRY /OUT:%s",
+                          "tests/test.lib", "/EXPORT:greeting", "temp.dll");
+
         HMODULE handle = LoadLibrary("temp.dll");
         FARPROC dyn_proc = GetProcAddress(handle, "greeting");
 
